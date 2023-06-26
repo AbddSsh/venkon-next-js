da03cc1641af062ac868efda6c00b8786af3e196
abd_sh <abduraimovshakh@gmail.com>
2023-06-26 15:44:54 +0500

    whyus prod

--- services/getData.js
+++ services/getData.js
@@ -4,7 +4,7 @@ const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 export const getSecondWhyWeData = async (id, lang) => {
   const { data } = await axios.get(
-    `https://laappetit.uz/vencon/user/page?page_id=${id}&language=${lang}`
+    `${baseUrl}/vencon/user/page?page_id=${id}&language=${lang}`
   );
   return data;
 };
